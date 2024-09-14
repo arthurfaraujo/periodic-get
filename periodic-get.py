86d881537a9aaef736dac2f08249229aa8b667aa
Arthur Felipe <92448606+arthurfaraujo@users.noreply.github.com>
2024-09-14 09:31:51 -0300

    Update periodic-get.py

--- periodic-get.py
+++ periodic-get.py
@@ -1,7 +1,7 @@
-import request
+import requests
 
 def make_request():
-  response = request.get("https://si-activities-api.onrender.com/api/activities")
+  response = requests.get("https://si-activities-api.onrender.com/api/activities")
   print(response.status_code, response.text)
 
 if __name__ == "__main__":
