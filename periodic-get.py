fbcc0c0658634539096e3e5756cd6b2b95654f10
arthur <arthurfelipe5567@gmail.com>
2024-09-16 19:07:24 -0300

    remove python

--- periodic-get.py
+++
@@ -1,5 +0,0 @@
-import subprocess
-
-# Inicia o subprocesso em background
-subprocess.Popen(['python', '-c', 'import requests; requests.get(\"https://si-activities-api.onrender.com/api/activities\")'], stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL, start_new_session=True)
-print('Requisição enviada.')
